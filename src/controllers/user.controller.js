a83b7ed793bf315573ab575f1947c2d1d726eca1
jesusrpd <jesusrodrigo881@gmail.com>
2021-05-24 12:01:42 -0500

    Correction res in the request login and signup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import config from '../config';
 
 //Create user
 export const signup = async (req, res)=>{
-    const {username, email, password} = req.body;
+    const {username, email, password, check} = req.body;
     const newUser = new User({
         username, email, password
     });
@@ -12,16 +12,15 @@ export const signup = async (req, res)=>{
     newUser.password = await newUser.encryptPassword(password);
     await newUser.save();
     //Generated token
-    const token = jwt.sign({id: newUser._id}, config.SECRET, {
-        expiresIn: 86400
-    });
+    const sesion = check ? null: {expiresIn: 86400};
+    const token = jwt.sign({id: newUser._id}, config.SECRET, sesion);
 
-    res.status(200).json({token, auth: true});
+    res.status(200).json({token, auth: true, username: newUser.username});
 };
 
 //Login user
 export const login = async (req, res)=>{
-    const {username, password} = req.body;
+    const {username, password, check} = req.body;
 
     const user = await User.findOne({username});
     if(!user) return res.status(404).json({message: 'User not found'});
@@ -29,8 +28,7 @@ export const login = async (req, res)=>{
     const passwordIsValid = await user.matchPassword(password);
     if(!passwordIsValid) return res.status(502).json({message: 'Password is invalid'});
     //Generated token
-    const token = jwt.sign({id: user._id}, config.SECRET, {
-        expiresIn: 86400
-    });
-    res.status(200).json({token, auth: true});
+    const sesion = check ? null: {expiresIn: 86400}
+    const token = jwt.sign({id: user._id}, config.SECRET,sesion);
+    res.status(200).json({token, auth: true, username: user.username});
 };
